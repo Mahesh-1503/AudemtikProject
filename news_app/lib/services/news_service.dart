e5f7a3a0471750d982ec50fbef0272cb9bb1d5be
Mahesh-1503 <mnopxyz090@gmail.com>
2025-05-10 15:02:30 +0530

    news services

---
+++ news_app/lib/services/news_service.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/article_model.dart';
+
+class NewsService {
+  static const String _apiKey = 'YOUR_API_KEY';
+  static const String _baseUrl =
+      'https://newsapi.org/v2/top-headlines?country=in&apiKey=$_apiKey';
+
+  static Future<List<Article>> fetchNews() async {
+    final response = await http.get(Uri.parse(_baseUrl));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List articles = data['articles'];
+      return articles.map((json) => Article.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load news');
+    }
+  }
+}

e19710c047c92e9e8be68633cc19ec7aff3f741f
Mahesh-1503 <mnopxyz090@gmail.com>
2025-05-10 15:11:18 +0530

    did needed modifcations

--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -4,14 +4,17 @@ import 'pages/home_page.dart';
 import 'bindings/home_binding.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'News Aggregator',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
       debugShowCheckedModeBanner: false,
       initialBinding: HomeBinding(),
       home: HomePage(),
